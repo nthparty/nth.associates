040a05e5e5de0f456c91201d1be6732af57cd370
Andrei Lapets <lapets@gmail.com>
2021-02-23 23:45:22 -0500

    Add linting rules, fix linting issues, and update README.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # nth.associates
 Client-side and service-side components for a secure data association and enrichment service.
+
+## Conventions
+
+Style conventions are enforced using [Pylint](https://www.pylint.org/):
+```bash
+pylint nthassociates
+```
--- nthassociates.py
+++ nthassociates.py
@@ -11,20 +11,31 @@ import oblivious
 import bcl
 
 class protocol:
+    """
+    Functions to be applied in parallel to data sets
+    being processed at each step of the protocol.
+    """
     @staticmethod
     def mask_client_row(value_scalar):
-        (value, session) = value_scalar
-        point = oblivious.point(base64.standard_b64decode(value)) 
-        return [[base64.standard_b64encode(session.scalar * point).decode('utf-8')]]
+        """
+        Mask each row supplied by the client.
+        """
+        (value, session_) = value_scalar
+        point = oblivious.point(base64.standard_b64decode(value))
+        return [[base64.standard_b64encode(session_.scalar * point).decode('utf-8')]]
 
     @staticmethod
     def enrich_client_row(value_scalar):
-        (value, session) = value_scalar
+        """
+        Extend each client row with new (encrypted) data and a masked
+        key for decrypting it.
+        """
+        (value, session_) = value_scalar
         secret_key = oblivious.point()
-        secret_key_masked = session.scalar_for_key * secret_key
+        secret_key_masked = session_.scalar_for_key * secret_key
         point = oblivious.point.hash(value[0].encode())
         row = value
-        row[0] = base64.standard_b64encode(session.scalar * point).decode('utf-8')
+        row[0] = base64.standard_b64encode(session_.scalar * point).decode('utf-8')
         row.extend([
             base64.standard_b64encode(secret_key_masked).decode('utf-8'),
             base64.standard_b64encode(
@@ -38,29 +49,37 @@ class protocol:
 
     @staticmethod
     def unlock_service_row_key(value_scalar):
-        ((p, k), session) = value_scalar
+        """
+        Unmask the key corresponding to a matching row, allowing
+        the receiving party to decrypt matching rows.
+        """
+        ((p, k), session_) = value_scalar
         return [[
             base64.standard_b64encode(
-                ~session.scalar * oblivious.point(base64.standard_b64decode(p))
-            ).decode('utf-8'), 
+                ~session_.scalar * oblivious.point(base64.standard_b64decode(p))
+            ).decode('utf-8'),
             base64.standard_b64encode(
-                ~session.scalar_for_key * oblivious.point(base64.standard_b64decode(k))
+                ~session_.scalar_for_key * oblivious.point(base64.standard_b64decode(k))
             ).decode('utf-8')
         ]]
 
     @staticmethod
-    def reply(session, request):
+    def reply(session_, request):
+        """
+        Handler for HTTP requests supplied by the client
+        in JSON format (for each protocol step).
+        """
         if 'step_one' in request:
             req = request['step_one']
 
             data_masked_at_cli_masked_at_srv = mr4mp.mapconcat(
                 protocol.mask_client_row,
-                [(v, session) for v in req['data@cli;masked@cli']]
+                [(v, session_) for v in req['data@cli;masked@cli']]
             )
 
             data_srv_masked = mr4mp.mapconcat(
                 protocol.enrich_client_row,
-                [(v, session) for v in session.data]
+                [(v, session_) for v in session_.data]
             )
 
             return {
@@ -75,7 +94,7 @@ class protocol:
 
             data_srv = mr4mp.mapconcat(
                 protocol.unlock_service_row_key,
-                [(v, session) for v in data_srv]
+                [(v, session_) for v in data_srv]
             )
 
             data_srv = [[p, k] for [p, k] in data_srv if p in req["data@cli;masked@cli"]]
@@ -84,13 +103,19 @@ class protocol:
 
         return None
 
-class session:
+class session: # pylint: disable=R0903
+    """
+    Data structure for an enrichment session.
+    """
     def __init__(self, path=None):
+        """
+        Create a session instance.
+        """
         self.scalar = oblivious.scalar()
         self.scalar_for_key = oblivious.scalar()
         self.path = path
         self.data = None
-        
+
         if self.path is not None:
             with open(self.path) as path_file:
                 self.data = json.load(path_file)
